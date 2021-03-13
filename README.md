4d57bcbe8cba900b98df87bac102163dc7019952
Ding Fei <fei.ding@ustchcs.com>
2021-03-13 16:26:21 +0800

    add build scripts

---
+++ README.md
@@ -0,0 +1,3 @@
+Build
+=====
+Refer to `.gitlab-ci.yml` for prerequisites, run `scripts/build.sh`.
---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+function main() {
+  local script_home=$(dirname $(realpath "$0"))
+
+  "${script_home}"/gen.figure.sh
+  "${script_home}"/gen.toc.sh
+
+  xelatex cpp20.tex
+  xelatex cpp20.tex
+}
+
+main "$@"
